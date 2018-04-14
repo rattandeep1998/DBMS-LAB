0c7fedfa885a03564f05b53c7bfab2de1b6b3c24
rajat singhal <rajatsinghal1997@gmail.com>
2018-04-14 20:05:37 +0530

    creating student functions

--- flask/DBapp/hello.py
+++ flask/DBapp/hello.py
@@ -63,25 +63,25 @@ def instructor_courses(iid, cid):
 		'videos_problems':[],
 	}
 
-	cursor.execute("SELECT SID, DATE FROM ENROLLS WHERE CID = " + cid)
+	cursor.execute("SELECT SID, DATE FROM enrolls WHERE CID = " + cid)
 	students = cursor.fetchall()
 
 	for s in students:
 		d['students'].append({'sid':s[0],'date':s[1]})
 
-	cursor.execute("SELECT SNAME, RATING, DESCRIPTION, DATE FROM FEEDBACK, STUDENT WHERE CID = " + cid + "AND FEEDBACK.SID = STUDENT.SID")
+	cursor.execute("SELECT SNAME, RATING, DESCRIPTION, DATE FROM feedback, student WHERE CID = " + cid + "AND feedback.SID = student.SID")
 	feedbacks = cursor.fetchall()
 
 	for f in feedbacks:
 		d['feedbacks'].append({'sname':f[0],'rating':f[1],'description':f[2],'date':f[3]})
 
-	cursor.execute("SELECT VID, TOPIC FROM TAKES NATURAL JOIN COURSE_VIDEO WHERE IID = " + iid + "AND CID = " + cid)
+	cursor.execute("SELECT VID, TOPIC FROM TAKES NATURAL JOIN course_video WHERE IID = " + iid + "AND CID = " + cid)
 	videos_problems = cursor.fetchall()
 
 	for v in videos_problems:
 		d['videos_problems'].append({'vid':v[0],'topic':v[1],'problems':[]})
 		l = len(d['videos_problems'])
-		cursor.execute("SELECT PID,QUESTION,OPT1,OPT2,OPT3,OPT4,CORRECT FROM PROBLEM WHERE VID = " + str(v[0]))
+		cursor.execute("SELECT PID,QUESTION,OPT1,OPT2,OPT3,OPT4,CORRECT FROM problem WHERE VID = " + str(v[0]))
 		problems = cursor.fetchall()
 		for p in problems:
 			d['videos_problems'][l-1]['problems'].append({'pid':p[0],'question':p[1],'opt1':p[2],'opt2':p[3],'opt3':p[4],'opt4':p[5],'correct':p[6]})
@@ -105,35 +105,71 @@ def add_problem(iid,cid):
 def edit_problem(iid,cid,pid):
 	pass
 
+@app.route('/student_signup', methods=['GET', 'POST'])
+def student_signup():
+	return render_template('student_signup.html', message=request.args.get('message'))
 
+@app.route('/student_register', methods=['POST'])
+def student_register():
+	name = request.form['name']
+	dob = request.form['dob']
+	address = request.form['address']
+	phone_number = request.form['phone_number']
+	highest_degree = request.form['highest_degree']
+	email = request.form['email']
+	password = request.form['password']
 
+	print(name, dob, address, phone_number, highest_degree, email, password)
+	cursor.execute("SELECT sid from student where email='" + email + "'")
+	f = cursor.fetchone()
+	if f is not None:
+		return redirect(url_for('student_signup', message='User already exists'))
+	
+	cursor.execute("SELECT COUNT(*) FROM student")
+	count = cursor.fetchone()[0]
+	data = (count+1,name,str(dob),address,phone_number,highest_degree,email,password)
+	query = "INSERT INTO student VALUES ("+str(count+1)+",'"+name+"','"+dob+"','"+address+"',"+phone_number+",'"+highest_degree+"','"+email+"','"+password+"')"
+	print(query)
+	cursor.execute(query)
+	cusor.connection.commit()
+	print('-----')
+	return redirect('/student/'+str(count+1), code=307)
 
-# @app.route("/Authenticate")
-# def Authenticate():
-#     email = request.args.get('email')
-#     password = request.args.get('password')
-#     cursor = mysql.connect().cursor()
-#     cursor.execute("SELECT * from instructor where email='" + email + "' and password='" + password + "'")
-#     data = cursor.fetchone()
-#     print 'email' + email
-#     print 'password' + password
+@app.route('/student_login', methods=['GET', 'POST'])
+def student_login():
+	return render_template('student_login.html', message=request.args.get('message'))
 
-#     if data is None:
-#         return "Email or Password is wrong"
-#     else:
-#         return "Logged in successfully"
+@app.route('/student_login_auth', methods = ['POST'])
+def student_login_auth():
+	email = request.form['email']
+	password = request.form['password']
+	print(email, password)
+	cursor.execute("SELECT sid from student where email='" + email + "' and password='" + password + "'")
+	data = cursor.fetchone()
+	print(data)
+	if data is None:
+		return redirect(url_for('student_login', message='Email or Password is invalid'))
+	else:
+		return redirect('/student/'+str(data[0]), code=307)
 
-# @app.route('/signUp')
-# def signUp():
-#     return render_template('signUp.html')
+@app.route('/student/<sid>', methods=['POST'])
+def student_dashboard(sid):
+	print('sid = ', sid)
+	cursor.execute("SELECT cid, date FROM enrolls where sid=" + sid)
+	courses = cursor.fetchall()
 
+	for c in courses:
+		pass
 
-# @app.route('/signUpUser', methods=['POST'])
-# def signUpUser():
-#     user =  request.form['username']
-#     password = request.form['password']
-    
-#     return json.dumps({'status':'OK','user':user,'pass':password})
+	return sid
+	# d={
+	# 	'iid':iid,
+	# 	'name':name,
+	# 	'cid':[],
+	# 	'cname':[]
+	# }
+
+	# return render_template('student_dashboard.html', data=d)
 
 if __name__ == "__main__":
 	app.run(port=8000, debug=True)
