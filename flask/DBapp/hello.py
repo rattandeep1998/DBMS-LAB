ca1655c9af2434728c69c138e7801fc4439cea34
rattandeep1998 <merattandeep@gmail.com>
2018-04-18 16:22:28 +0530

    local video can be added

--- flask/DBapp/hello.py
+++ flask/DBapp/hello.py
@@ -5,7 +5,7 @@ import json, datetime
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'rattandeep1998'
 app.config['MYSQL_DATABASE_DB'] = 'AppData'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
